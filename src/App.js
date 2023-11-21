cf679d7920065db34b17cef7bf6ebc9746756202
HaashiniA <haasteriar424@gmail.com>
2023-11-21 16:05:15 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <List2/> */}
+      {/* <Key/> */}
+      {/* <List/> */}
+      {/* <PrimarySearchAppBar/> */}
+      {/* <FormPropsTextFields/> */}
+      {/* <RatingSize/> */}
+      {/* <FormDialog/> */}
+      {/* <PositionedSnackbar/> */}
+      {/* <SimpleBackdrop/> */}
+      {/* <Day4cwhelp1/> */}
+      {/* <Day4cwhelp2/> */}
+      {/* <Day4cwhelp3/> */}
+      {/* <Day4cw/> */}
+      {/* <Hooksconcept/> */}
+      {/* <PropsFunc name="Imran" age="18" /> */}
+      {/* <MyComponent/> */}
+      {/* <Ques20/> */}
+      {/* <Ques32/> */}
+      {/* <Ques30/> */}
+      {/* <ErrorBoundary2>
+        <ErrorBoundary1 studentName="Harish"/>
+      </ErrorBoundary2> */}
+      {/* <TryCatch name='Aja'/> */}
+      {/* <Loginpage/> */}
+      {/* <HoverCouunter/> */}
+      {/* <Propvalidation name={12}/> */}
+      {/* <SpreadpassUnkonown title1='SKCET' type='password' placeholder='Enter Password' id='i'/> */}
+      {/* <Reference/> */}
+      {/* <BrowserRouter>
+      <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+       </ul> */}
+       {/* <Navbar/> */}
+          {/* <Routes>
+            <Route path='/Home' element={<Home/>}></Route>
+            <Route path='/About' element={<About/>}></Route>
+          </Routes>
+      </BrowserRouter> */}
+      {/* <Day4PAH/> */}
+      {/* <AxiosGet/> */}
+      {/* <PostAxios/> */}
+      {/* <DelAxios/> */}
+      {/* <BrowserRouter>
+       <Day7cw/>
+          <Routes>
+            <Route path='/home' element={<Day7cwhelp1/>}></Route>
+            <Route path='/singers' element={<Day7cwhelp2/>}></Route>
+            <Route path='/album' element={<Day7cwhelp3/>}></Route>
+          </Routes>
+      </BrowserRouter> */}
+      {/* <BrowserRouter>
+       <Day7pah/>
+          <Routes>
+            <Route path='/home' element={<Home/>}></Route>
+            <Route path='/about' element={<About/>}></Route>
+          </Routes>
+      </BrowserRouter> */}
+      {/* <Day8cw/> */}
+      {/* <Day8Pah/> */}
     </div>
   );
 }
 
-export default App;
